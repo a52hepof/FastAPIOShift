4c4ae947dff1c3ba6f9549c8e488cd470af6f5f4
Fernando <a52hepof@uco.es>
2022-10-16 18:13:38 +0200

    modified endpoints

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': 'Hello FastAPI!'}
+    return {'data': 'Hello  FastAPI!'}
 
 @app.get('/message')
 def index():
-    return {'data': 'FastAPI is great!'}
+    return {
+    	'año': '2022',
+    	'curso': '3º',
+    	'asignatura':'gsad',
+    	'alumno':'a52hepof'
+
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080)
